1c5db31b040e81c4bd9052f1a69326cb64e64041
Jon Poole <jonfpoole@gmail.com>
2024-07-10 14:16:48 +0100

    Fix some typos

--- src/main/main.c
+++ src/main/main.c
@@ -11,11 +11,11 @@ int main(void)
     printf("Hello, main!\n");
     printf("If you're using qemu, to get your mouse back, press ctr-alt or ctr-alt-g\n");
 
-    // These should be allocated sequentially i.e. headchar2 should be 100 bytes after heapchars1
+    // These should be allocated sequentially i.e. heap_chars2 should be 100 bytes after heap_chars1
     char* heap_chars1 = malloc(100 * sizeof(char));
     char* heap_chars2 = malloc(100 * sizeof(char));
     free(heap_chars1);
-    // This should be able to re-use the same memory from heapchars1
+    // This should be able to re-use the same memory from heap_chars1
     char* heap_chars3 = malloc(100 * sizeof(char));
     printf("heap chars: %lu %lu %lu\n", (unsigned long)heap_chars1, (unsigned long)heap_chars2, (unsigned long)heap_chars3);
     return 0;
