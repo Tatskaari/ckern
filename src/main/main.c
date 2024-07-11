982ef090cb15d07549d36680c93170a90a19ac53
Jonathan Poole <jonfpoole@gmail.com>
2024-07-11 11:53:20 +0100

    Update main.c

--- src/main/main.c
+++ src/main/main.c
@@ -17,7 +17,7 @@ int main(void) {
     // Posix expects these registered at these file handles.
     register_file_handle(0, "/dev/stdin", NULL, NULL);
     register_file_handle(1, "/dev/stdout", NULL, terminal_write);
-    register_file_handle(2, "/dec/stderr", NULL, NULL);
+    register_file_handle(2, "/dev/stderr", NULL, NULL);
 
     printf("This should print to the vga term\n");
 
