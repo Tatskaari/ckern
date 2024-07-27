bf26f35beba5902c66b7b1c39c32fd490bd345c3
Jon Poole <jonfpoole@gmail.com>
2024-07-27 18:14:14 +0100

    Port more of the serial stuff

--- zig/libc.zig
+++ zig/libc.zig
@@ -1,1 +1,2 @@
+// These are functions that will be available when we're linked into the C binary
 pub extern fn printf(format: [*:0]const u8, ...) c_int;
--- zig/serial.zig
+++ zig/serial.zig
@@ -14,17 +14,11 @@ pub const COM2: u16 = 0x2F8;
 pub const COM3: u16 = 0x3E8;
 pub const COM4: u16 = 0x2E8;
 
-extern fn serial_write(port: u16, data: [*]const u8, count: usize) usize;
-extern fn serial_init(port: u16) usize;
-
 pub const SerialPortFile = struct {
     Port: u16,
 
     pub fn init(self: *SerialPortFile) !void {
-        const result = serial_init(self.Port);
-        if (result != 0) {
-            return SerialError.InitFailed;
-        }
+        try serial_init(self.Port);
     }
 
     pub fn read(self: *SerialPortFile, buffer: []u8, count: usize) !usize {
@@ -54,8 +48,8 @@ pub fn sgetc(port: u16) u8 {
     return libasm.inb(port);
 }
 
-pub fn serial_read(port: u16, dest: []u8, count: u32 ) u32 {
-    for(0..count) |i| {
+pub fn serial_read(port: u16, dest: []u8, count: usize) usize {
+    for (0..count) |i| {
         dest[i] = sgetc(port);
     }
     // We always read count, but this conforms to the file writing signature which makes posix-ing more easier
@@ -63,3 +57,41 @@ pub fn serial_read(port: u16, dest: []u8, count: u32 ) u32 {
     return count;
 }
 
+fn is_transmit_empty(port: u16) bool {
+    return libasm.inb(port + 5) & 0x20 == 0;
+}
+
+fn writec(port: u16, c: u8) void {
+    while (is_transmit_empty(port)) {}
+    libasm.outb(port, c);
+}
+
+fn serial_write(port: u16, data: [*]const u8, count: usize) usize {
+    for (0..count) |i| {
+        writec(port, data[i]);
+    }
+    return count;
+}
+
+fn serial_init(port: u16) !void {
+    // TODO use bitmasks because the tutorial is horrible
+    libasm.outb(port + 1, 0x00); // Disable all interrupts
+    libasm.outb(port + 3, 0x80); // Enable DLAB (set baud rate divisor)
+    libasm.outb(port + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
+    libasm.outb(port + 1, 0x00); //                  (hi byte)
+    libasm.outb(port + 3, 0x03); // 8 bits, no parity, one stop bit
+    libasm.outb(port + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
+    libasm.outb(port + 4, 0x0B); // IRQs enabled, RTS/DSR set
+
+    libasm.outb(port + 4, 0x1E); // Set in loopback mode, test the serial chip
+    libasm.outb(port + 0, 0xAE); // Test serial chip (send byte 0xAE and check if serial returns same byte)
+
+    // Check if serial is faulty (i.e: not same byte as sent)
+    if (libasm.inb(port + 0) != 0xAE) {
+        return SerialError.InitFailed;
+    }
+
+    // If serial is not faulty set it in normal operation mode
+    // (not-loopback with IRQs enabled and OUT#1 and OUT#2 bits enabled)
+    libasm.outb(port + 4, 0x0F);
+}
