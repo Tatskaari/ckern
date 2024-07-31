d91695bb3c99d6d9a288eb1784dd7b4cdfe312ce
Jon Poole <jonfpoole@gmail.com>
2024-07-31 21:11:49 +0100

    Move memory management into Zig

--- zig/mem.zig
+++ zig/mem.zig
@@ -7,6 +7,16 @@ pub var alloc : std.heap.FixedBufferAllocator = undefined;
 
 const MEM_SIZE = 1024 * 256; // 256kb
 
+extern const kernel_end: u8;
+var kernel_brk = &kernel_end;
+
+export fn sbrk( increment: usize) *u8 {
+    const last_brk = kernel_brk;
+    kernel_brk = @ptrFromInt(@intFromPtr(kernel_brk) + increment);
+    // This isn't really a pointer to a u8. It's a watermark in memory so we need to tell zig to trust us here.
+    return @constCast(last_brk);
+}
+
 pub fn init() void {
     const heap = malloc(MEM_SIZE)[0..MEM_SIZE];
     alloc = std.heap.FixedBufferAllocator.init(heap);
