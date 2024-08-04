e7b06bcfec88bca285cd829aadcababa274a4224
Jon Poole <jonfpoole@gmail.com>
2024-08-04 20:51:34 +0100

    Confirm we're on multiboot1

--- src/main.zig
+++ src/main.zig
@@ -22,21 +22,23 @@ export var multiboot align(4) linksection(".multiboot") = MultibootHeader{
 const STACK_SIZE = 16 * 2024;
 export var stack: [STACK_SIZE]u8 align(16) linksection(".bss") = undefined;
 export fn _start() linksection(".text") callconv(.Naked) noreturn {
-    const asmStr = std.fmt.comptimePrint(
-        \\ lea stack + {}, %esp
-        \\ call main
-    , .{STACK_SIZE});
-    asm volatile (asmStr
-        :
-        : [stk] "r" (@intFromPtr(&stack) + @sizeOf(@TypeOf(stack))),
-        : "esp"
+    asm volatile (std.fmt.comptimePrint(
+    \\ lea stack + {}, %esp
+    \\ push %eax
+    \\ call main
+, .{STACK_SIZE}) ::: "esp"
     );
     while (true) {}
 }
 
-export fn main() i32 {
+const MULTI_BOOT_MAGIC = 0x2BADB002;
+export fn main(multiBootMagic: u32) i32 {
     arch.init();
     kernel.init();
+
+    if (multiBootMagic != MULTI_BOOT_MAGIC) {
+        kernel.terminal.print("Failed multiboot handshake. Functionality will be limitted. Expected magic number: 0x{x}, got 0x{x}", .{MULTI_BOOT_MAGIC, multiBootMagic});
+    }
 
     drivers.pci.lspci();
     // This should trigger our custom interupt!
